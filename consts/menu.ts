770e7231223d336d90cacd6601086b026be39fc8
Diego Gonzalez <diego.ags04@gmail.com>
2021-12-17 16:39:57 -0400

    Changes in menu

---
+++ consts/menu.ts
@@ -0,0 +1,214 @@
+export const menuData = [
+  {
+    sectionTitle: "Entradas",
+    food: [
+      {
+        name: "Mini Tequeños",
+        description: "Servicio de 6 mini tequeños y salsa de la casa",
+        price: 1,
+      },
+      {
+        name: "Tequeños",
+        description: "Servicio de 5 tequeños y salsa de la casa",
+        price: 2.5,
+      },
+      {
+        name: "Papas especiales",
+        description:
+          "Papas fritas importadas (300gr) con queso cheddar y tocineta",
+        price: 3,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Hamburguesas",
+    food: [
+      {
+        name: "Smokey Clásica",
+        description:
+          "Carne de la casa, pan de hamburguesa, queso cebú y papas rayadas",
+        price: 1.5,
+      },
+      {
+        name: "Smokey Especial de Carne",
+        description:
+          "Doble carne de la casa (160gr), pan wooper, doble queso cebú, tocineta, jamón, queso amarillo y servicio de papas importadas",
+        price: 6,
+      },
+      {
+        name: "Smokey Especial de Pollo",
+        description:
+          "Pollo de la casa, pan wooper, doble queso cebú, tocineta, jamón, queso amarillo y servicio de papas importadas",
+        price: 6,
+      },
+      {
+        name: "Smokey Crispy",
+        description:
+          "Pollo crispy de la casa, pan wooper, queso cebú, tocineta, jamón, doble queso amarillo y servicio de papas importadas",
+        price: 6.5,
+      },
+      {
+        name: "Smokey Mixta",
+        description:
+          "Pollo y carne de la casa, pan wooper, queso cebú, tocineta, jamón, queso amarillo y servicio de papas importadas",
+        price: 6.5,
+      },
+      {
+        name: "Smokey de la House",
+        description:
+          "Punta trasera, pan wooper, full queso cebú, tocineta, jamón ahumado, doble queso amarillo y servicio de papas importadas",
+        price: 7,
+      },
+      {
+        name: "Smokey Pernil",
+        description:
+          "Pernil de la casa, pan wooper, queso cebú, jamón, queso amarillo y servicio de papas importadas",
+        price: 7,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Hot Dogs",
+    food: [
+      {
+        name: "Hot Dog Clásico",
+        description:
+          "Salchicha polaca, queso cebú, pepinillos, pan de perro caliente",
+        price: 3,
+      },
+      {
+        name: "Hot Dog de la House",
+        description:
+          "Salchica polaca, pan de perro caliente, queso cheddar, tocineta, pepinillos y papas importadas",
+        price: 5,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Pizzas",
+    food: [
+      {
+        name: "Pizza New York",
+        description:
+          "Pizza extra grande con salsa roja de la casa y queso",
+        price: 5,
+      },
+      {
+        name: "Pizza One Taste",
+        description:
+          "Pizza familiar con salsa roja de la casa, y un ingrediente",
+        price: 6.5,
+      },
+      {
+        name: "Pizza Double Taste",
+        description:
+          "Pizza familiar con salsa roja de la casa, y dos ingredientes",
+        price: 8,
+      },
+      {
+        name: "Smokey Pizza de la House",
+        description:
+          "Salsa roja de la casa, Pepperoni, tocineta, salchichón y jamón ahumado",
+        price: 12,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Parrillas",
+    food: [
+      {
+        name: "Parrilla mixta",
+        description:
+          "Parrilla mixta para dos personas con pollo asado, punta trasera, chorizo ahumado, servicio de arepas, ensalada coleslaw y queso",
+        price: 6,
+      },
+      {
+        name: "Parrilla de carne",
+        description:
+          "Parrilla de carne para una persona con punta trasera, chorizo ahumado, servicio de arepas, ensalada coleslaw, queso y servico de papas especiales",
+        price: 5,
+      },
+      {
+        name: "Parrilla de pollo",
+        description:
+          "Parrilla de pollo para una person con pollo asado, chorizo ahumado, servicio de arepas, ensalada coleslaw, queso y servico de papas especiales",
+        price: 5,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Combos",
+    food: [
+      {
+        name: "Burgers Dúo",
+        description: "2 Smokey Especial de carne o pollo con papas + 2 refrescos 350ml",
+        price: 12,
+      },
+      {
+        name: "Burgers con panas",
+        description: "4 Smokey Especial de carne o pollo con papas + 1 refresco 1.5lts + 12 mini tequeños con salsa",
+        price: 25,
+      },
+      {
+        name: "Burgers cervecero",
+        description: "4 Smokey Clásicas + 1 balde de 12 cervezas",
+        price: 15,
+      },
+      {
+        name: "Papas cervecero",
+        description: "1 servicio de papas especiales + 1 balde de 12 cervezas",
+        price: 12.5,
+      },
+      {
+        name: "Una familiar",
+        description: "1 Pizza familiar (2 ingredientes, Double Taste) + refresco 1.5lts + 6 mini tequeños con salsa",
+        price: 10,
+      },
+      {
+        name: "2x One Taste",
+        description: "2 Pizzas Familiares (1 ingrediente, One Taste)",
+        price: 12,
+      },
+      {
+        name: "2x Double Taste",
+        description: "2 Pizzas familiares (2 ingredientes, Double Taste) + refresco 1.5lts + 12 mini tequeños y salsa",
+        price: 18,
+      },
+      {
+        name: "3x Double Taste",
+        description: "3 Pizzas familiares (2 ingredientes, Double Taste) + 2 refrescos 1.5lts + 12 mini tequeños y salsa",
+        price: 25,
+      },
+      {
+        name: "Parrilla Dúo",
+        description: "1 Parrilla mixta + 2 Smokey Clásicas + 2 refrescos 350ml",
+        price: 9,
+      },
+      {
+        name: "Aguita de sapo",
+        description: "2 arepitas de pernil + refresco 350ml",
+        price: 3.50,
+      },
+    ],
+  },
+  {
+    sectionTitle: "Bebidas",
+    food: [
+      {
+        name: "Cocacola 1.5L",
+        description: "",
+        price: 1.5,
+      },
+      {
+        name: "Cocacola 350ml",
+        description: "",
+        price: 0.75,
+      },
+      {
+        name: "Agua mineral",
+        description: "",
+        price: 0.6,
+      },
+    ],
+  },
+];
